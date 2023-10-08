d7cc7f59f16da21d71d8d891033684f60a80e761
aleleotta <114069815+aleleotta@users.noreply.github.com>
2023-10-08 20:00:34 +0200

    .

--- Tema 0 - Clases/Ejercicio 1/CuentaCorriente.py
+++ Tema 0 - Clases/Ejercicio 1/CuentaCorriente.py
@@ -9,5 +9,29 @@ class CuentaCorriente:
         self.dni = dni
         self.nombre = nombre
         self.saldo = saldo
+    
+    def ingresarDinero(self, saldo):
+        self.saldo = self.saldo + saldo
 
-    def sacarDinero(self, saldo)
+    def sacarDinero(self, saldo):
+        available = True
+        if self.saldo > 0:
+            self.saldo = self.saldo - saldo
+        else:
+            available = False
+        return available
+    
+    def __str__(self) -> str:
+        return "DNI: " + str(self.dni) + " Name: " + self.nombre + " Balance: " + str(self.saldo) + "\n"
+    
+    def __eq__(self, obj) -> bool:
+        equal = False
+        if self.dni == obj.dni:
+            equal = True
+        return equal
+    
+    def __lt__ (self, obj) -> bool:
+        menor = False
+        if self.saldo < obj.saldo:
+            menor = True
+        return menor
--- Tema 0 - Clases/Ejercicio 1/Main.py
+++ Tema 0 - Clases/Ejercicio 1/Main.py
@@ -13,3 +13,56 @@ Crear también los métodos __str__, __eq__ y __lt__.
 Se considera que dos cuentas corrientes son iguales si tienen el mismo DNI.
 Las cuentas corrientes se ordenarán de menor a mayor por el saldo.
 """
+
+from CuentaCorriente import *
+
+class Main:
+
+    list = []
+
+    def list():
+        print("1) Add bank account")
+        print("2) Show all bank accounts")
+        print("3) Remove bank account")
+        print("0) Exit")
+        print("\nChoose one of the options above:")
+
+    def add():
+        dni = 0
+        hasName = 0
+        name = ""
+        balance = 0
+        dni = int(input("Introduce your DNI: "))
+        hasName = int(input("Do you wanna introduce your name? 0 = No, 1 = Yes"))
+        if hasName == 1:
+            name = input("Introduce your name: ")
+            obj = CuentaCorriente(dni, name, balance)
+            list.append(obj)
+        else:
+            obj = CuentaCorriente(dni, balance)
+            list.append(obj)
+
+    def showList():
+        for currentObj in list:
+            currentObj.__str__
+        print("\n")
+
+    def remove():
+        dni = 0
+        dni = int(input("Introduce the DNI of the account to remove from list: "))
+        for currentObj in list:
+            if currentObj.dni == dni:
+                list.remove(currentObj)
+
+    option = 4
+    while option != 0:
+        list()
+        option = int(print("Introduce an option: "))
+        if option == 1:
+            add()
+        if option == 2:
+            showList()
+        if option == 3:
+            remove()
+        if option == 0:
+            print("Have a great day!")
